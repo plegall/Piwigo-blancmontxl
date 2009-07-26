513b2e68b426c824bf5a2aeba0fbce8781879446
repie38 <repie38@piwigo.org>
2009-07-26 21:33:04 +0000

    import of the current version (2.0)
    git-svn-id: http://piwigo.org/svn/extensions/blancmontxl@3686 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,14 @@
+<?php
+$themeconf = array(
+  'template' => 'yoga',
+  'template_dir' => 'template/yoga',
+  'theme' => 'blancmontxl',
+  'icon_dir' => PHPWG_ROOT_PATH.'template/yoga/theme/blancmontxl/icon',
+   'mime_icon_dir' => PHPWG_ROOT_PATH.'template/yoga/icon/mimetypes/',
+  'local_head' => '<!--[if lt IE 7]>
+  <link rel="stylesheet" type="text/css" href="'.PHPWG_ROOT_PATH.'template/yoga/theme/blancmontxl/fix-ie5-ie6.css">
+<![endif]-->'
+);
+$lang['Theme: blancmontxl'] = 'Thème BlancMontXP By repié38.';
+
+?>
