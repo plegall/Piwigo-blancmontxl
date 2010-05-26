e2bcf34b61871c70fd30ebeadf1015296d7a5a4d
repie38 <repie38@piwigo.org>
2010-05-26 14:01:19 +0000

    change var name for better compliance (thanks to P@t)
    git-svn-id: http://piwigo.org/svn/extensions/blancmontxl@6374 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -1,39 +1,39 @@
 <?php
 /*
 Theme Name: BlancMont XL
-Version: 2.1.c
+Version: 2.1.d
 Description: Night theme
 Theme URI: http://piwigo.org/ext/extension_view.php?eid=232
 Author: repie38
 Author URI: 
 */
 
-global $config;
-$config = unserialize($conf['BlancmontXL']);
+
+$conf['BlancmontXL'] = unserialize($conf['BlancmontXL']);
 
 $themeconf = array(
   'parent'      => 'default',
   'local_head'  => 'local_head.tpl',
-  'icon_dir'    => 'themes/blancmontxl/icon/'. $config['icon_dir'] 
+  'icon_dir'    => 'themes/blancmontxl/icon/'. $conf['BlancmontXL']['icon_dir'] 
 );
 
 add_event_handler('loc_begin_page_header', 'set_bmxl_header');
 
 function set_bmxl_header()
 {
-  global $page, $config, $template;
+  global $page,$template,$conf;
 
   if (isset($page['body_id']) and $page['body_id'] == 'theCategoryPage')
   {
-    $header = isset($page['category']) ? $config['categories'] : $config['home'];
+    $header = isset($page['category']) ? $conf['BlancmontXL']['categories'] : $conf['BlancmontXL']['home'];
   }
   elseif (isset($page['body_id']) and $page['body_id'] == 'thePicturePage')
   {
-    $header = $config['picture'];
+    $header = $conf['BlancmontXL']['picture'];
   }
   else
   {
-    $header = $config['other'];
+    $header = $conf['BlancmontXL']['other'];
   }
   $template->assign('display_bmxl_banner', $header);
 }
