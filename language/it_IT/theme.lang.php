252aaea3c28efcd2ac91347155354737131dc672
riopwg <riopwg@piwigo.org>
2010-11-07 17:07:16 +0000

    language IT
    git-svn-id: http://piwigo.org/svn/extensions/blancmontxl@7680 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/theme.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['Display page banner on home page'] = 'Visualizza il banner sulla pagina principale';
+$lang['Display page banner on categories pages'] = 'Visualizza il banner sulla pagina delle categorie';
+$lang['Display page banner on picture page'] = 'Visualizza il banner sulla pagine delle foto';
+$lang['Display page banner on other pages'] = 'Visualizza banner sulle altre pagine';
+$lang['Use MontBlancXL icon set'] = 'Usare le icone di <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> (tema non necessario)';
+
+?>
