263083c223aa1235d1aa142c73b01a52119555ff
ddtddt <ddtddt@piwigo.org>
2011-01-12 14:23:33 +0000

    [extensions] - blancmontx -  add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/blancmontxl@8628 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/theme.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['Display page banner on home page'] = 'Rādīt lapas baneri mājas lapā';
+$lang['Display page banner on categories pages'] = 'Rādīt lapas baneri kategoriju lapā';
+$lang['Display page banner on picture page'] = 'Rādīt lapas baneri attēlu lapā';
+$lang['Display page banner on other pages'] = 'Rādīt lapas baneri citās lapās';
+$lang['Use MontBlancXL icon set'] = 'Lietot <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> ikonu kopumu (tēma nav nepieciešama)';
+
+?>
