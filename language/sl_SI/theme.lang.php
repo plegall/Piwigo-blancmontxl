0720586a4541475fe4ea0d52d86fc86b01dcc6ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-23 09:17:56 +0100

    Update sl_SI, thanks to ddtddt

---
+++ language/sl_SI/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Prikaži naslovnico strani (banner) na straneh kategorij';
+$lang['Display page banner on home page'] = 'Prikaži naslovnico strani (banner) na domači strani';
+$lang['Display page banner on other pages'] = 'Prikaži naslovnico strani (banner) na ostalih straneh';
+$lang['Display page banner on picture page'] = 'Prikaži naslovnico strani (banner) na strani fotografij';
+$lang['Use MontBlancXL icon set'] = 'Uporabite nabor ikon <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> (tema ni potrebna)';
