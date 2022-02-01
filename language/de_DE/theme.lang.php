84eddd4b8e2fbda86b70316d23320eea9dedca8b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-02-01 16:13:33 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/theme.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Seitenbanner auf Kategorieseiten anzeigen';
+$lang['Display page banner on home page'] = 'Seitenbanner auf der Startseite anzeigen';
+$lang['Display page banner on other pages'] = 'Seitenbanner auf anderen Seiten anzeigen
+';
+$lang['Display page banner on picture page'] = 'Seitenbanner auf der Bildseite anzeigen
+';
+$lang['Use MontBlancXL icon set'] = 'Verwenden Sie den <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a>-Symbolsatz (Thema nicht erforderlich)';
