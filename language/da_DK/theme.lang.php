6bd7235f36c51d4e0e6f8ce50276b736cc0a208a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-04 14:35:57 +0200

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Vis sidebanner på albumsider';
+$lang['Display page banner on home page'] = 'Vis sidebanner på forside';
+$lang['Display page banner on other pages'] = 'Vis sidebanner på andre sider';
+$lang['Display page banner on picture page'] = 'Vis sidebanner på billedsider';
+$lang['Use MontBlancXL icon set'] = 'Anvend ikonsættet <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> (tema ikke nødvendigt)';
