05148eb4c435625f57dd117b96972b300558067e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-06 06:16:14 +0200

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Use MontBlancXL icon set'] = 'Gebruik <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> icon set (thema niet nodig)';
+$lang['Display page banner on picture page'] = 'Paginabanner weergeven op fotopagina';
+$lang['Display page banner on other pages'] = 'Paginabanner op andere pagina\'s weergeven';
+$lang['Display page banner on home page'] = 'Paginabanner weergeven op startpagina';
+$lang['Display page banner on categories pages'] = 'Paginabanner weergeven op categoriepagina\'s';
