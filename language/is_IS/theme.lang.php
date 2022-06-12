6cedaa3f37ed8f03b61576ffb6811ba0bee865d6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-12 06:32:41 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Birta síðuborða á flokkasíðum';
+$lang['Display page banner on home page'] = 'Birta síðuborða á heimasíðunni';
+$lang['Display page banner on other pages'] = 'Birta síðuborða á öðrum síðum';
+$lang['Display page banner on picture page'] = 'Birta síðuborða á myndasíðu';
+$lang['Use MontBlancXL icon set'] = 'Nota <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> táknmyndasett (þema ekki nauðsynlegt)';
