c227841ebf977bfb9800adb361dc64c84ab24c23
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-08 13:12:49 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Exibir banner de página nas páginas de categorias';
+$lang['Display page banner on home page'] = 'Exibir banner da página na página inicial';
+$lang['Display page banner on other pages'] = 'Exibir banner de página em outras páginas';
+$lang['Display page banner on picture page'] = 'Exibir banner da página na página da imagem';
+$lang['Use MontBlancXL icon set'] = 'Use o conjunto de ícones <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> (o tema não é necessário)';
