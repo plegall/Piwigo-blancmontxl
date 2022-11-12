e3f4dbac93cf917508ffdc8f8ba4ceec08e67945
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-12 21:28:17 +0100

    Update cs_CZ, thanks to MiPi

---
+++ language/cs_CZ/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Zobrazit banner stránky na stránkách kategorií';
+$lang['Display page banner on home page'] = 'Zobrazit banner stránky na domovské stránce';
+$lang['Display page banner on other pages'] = 'Zobrazit banner stránky na ostatních stránkách';
+$lang['Display page banner on picture page'] = 'Zobrazit banner stránky na stránce obrázku';
+$lang['Use MontBlancXL icon set'] = 'Použij sadu ikon <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> (téma není nutné)';
