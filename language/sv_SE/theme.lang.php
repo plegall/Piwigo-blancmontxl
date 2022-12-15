c670b8de48f7fc7d510bdf049b5d11dd793dcf9f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-15 20:38:25 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Visa sidbanner på kategorisidor';
+$lang['Display page banner on home page'] = 'Visa sidbanner på hemsida';
+$lang['Display page banner on other pages'] = 'Visa sidbanner på andra sidor';
+$lang['Display page banner on picture page'] = 'Visa sidbanner på bildsida';
+$lang['Use MontBlancXL icon set'] = 'Använd <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> ikonuppsättning (tema ej nödvändigt)';
