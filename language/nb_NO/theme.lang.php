86b8f18af574f98f2bd448df1fa646ab2309c446
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-23 14:56:34 +0100

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Vis sidebanner på kategorisider';
+$lang['Display page banner on home page'] = 'Vis sidebanner på hjemmesiden';
+$lang['Display page banner on other pages'] = 'Vis sidebanner på andre sider';
+$lang['Display page banner on picture page'] = 'Vis sidebanner på bildeside';
+$lang['Use MontBlancXL icon set'] = 'Bruk <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> ikonsett (tema er ikke nødvendig)';
