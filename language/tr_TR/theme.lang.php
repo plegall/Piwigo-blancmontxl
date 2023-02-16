cac170d8062c8eb64bb69e2d0e3f21eb98878b77
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-16 20:50:15 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Use MontBlancXL icon set'] = '<a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> sembol setini kullan (tema gerekli değil)';
+$lang['Display page banner on categories pages'] = 'Kategoriler sayfasında afiş göster';
+$lang['Display page banner on home page'] = 'Ana sayfada afiş göster';
+$lang['Display page banner on other pages'] = 'Diğer sayfalarda afiş göster';
+$lang['Display page banner on picture page'] = 'Resim sayfasında afiş göster';
