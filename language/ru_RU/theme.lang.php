f36c199ad32df8046be6e81360905e32ff57db65
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:22:26 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Отображение баннера страницы на страницах категорий';
+$lang['Display page banner on home page'] = 'Отображение баннера страницы на главной странице';
+$lang['Display page banner on other pages'] = 'Отображать баннер страницы на других страницах';
+$lang['Display page banner on picture page'] = 'Отображение баннера страницы на странице с изображением';
+$lang['Use MontBlancXL icon set'] = 'Используйте набор значков <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> (тема не обязательна)';
