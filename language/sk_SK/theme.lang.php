af2269c74ec89e1b53c4e546cc536a219a8fbede
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-17 13:24:26 +0200

    Update sk_SK, thanks to dodo

---
+++ language/sk_SK/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Zobrazenie banneru stránky na stránkach kategórií';
+$lang['Display page banner on home page'] = 'Zobrazenie banneru stránky na domovskej stránke';
+$lang['Display page banner on other pages'] = 'Zobrazenie banneru stránky na iných stránkach';
+$lang['Display page banner on picture page'] = 'Zobrazenie banneru stránky na stránke s obrázkom';
+$lang['Use MontBlancXL icon set'] = 'Použite sadu ikon <a href="http://piwigo.org/ext/extension_view.php?eid=133">PontBlancXL</a> (téma nie je potrebná)';
