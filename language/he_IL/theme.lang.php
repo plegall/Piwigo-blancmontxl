a53b568287721ec7c47e4529b08b9e11fe943a85
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 06:30:13 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'הצג באנר עמוד בעמודי קטגוריות';
+$lang['Display page banner on home page'] = 'הצג באנר עמוד בעמוד הבית';
+$lang['Display page banner on other pages'] = 'הצג באנר עמוד בעמודים אחרים';
+$lang['Display page banner on picture page'] = 'הצג באנר עמוד בעמוד התמונה';
+$lang['Use MontBlancXL icon set'] = 'השתמש בסט צלמית <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> (נושא לא נדרש)';
