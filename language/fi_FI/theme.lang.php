a24d340ac710a040d04e85fa7e1c75b055a17f43
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-04-14 11:11:48 +0200

    Update fi_FI, thanks to Jusaus

---
+++ language/fi_FI/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = 'Näytä sivun banneri kategoriasivuilla';
+$lang['Display page banner on home page'] = 'Näytä sivun banneri etusivulla';
+$lang['Display page banner on other pages'] = 'Näytä sivun banneri muilla sivuilla';
+$lang['Display page banner on picture page'] = 'Näytä sivun banneri kuvasivulla';
+$lang['Use MontBlancXL icon set'] = 'Käytä <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> kuvakepakettia (teemaa ei tarvita)';
