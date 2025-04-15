d1b1379eb680e204c59f5521bf3b2a5c2730ca44
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-04-15 13:50:36 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/theme.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display page banner on categories pages'] = '在分類頁面上顯示頁面橫幅';
+$lang['Display page banner on home page'] = '在主頁上顯示頁面橫幅';
+$lang['Display page banner on other pages'] = '在其他頁面上顯示頁面橫幅';
+$lang['Display page banner on picture page'] = '在圖片頁面上顯示頁面橫幅';
+$lang['Use MontBlancXL icon set'] = '使用 <a href="http://piwigo.org/ext/extension_view.php?eid=133">MontBlancXL</a> 的圖示集（不需要佈景主題）';
